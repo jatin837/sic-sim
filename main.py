b1b8981d933977f891b4b32f51ec10474278b010
dj4t9n <pandeyjatin47@gmail.com>
2021-08-05 16:15:47 +0530

    arg parser

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import math as m
+import os
+import argparse
 
 class Node():
     def __init__(self, label, voltage=m.inf):
@@ -21,5 +23,29 @@ class Parser():
     def read_source(self):
         with open(self.source_path, 'r') as f:
             return f.read();
+    
+    def parse(self):
+        for comm in self.source.split("\n"):
+            if comm[:2] == '--':
+                pass
+            else:
+                pass
 
 
+def get_args() -> tuple:
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--input", required=True, help="Path to input source")
+    args = vars(ap.parse_args())
+
+    ipath = os.path.abspath(args["input"])
+    return ipath
+
+def main():
+    ipath: str = get_args()
+    parser = Parser(ipath)
+    print("-"*10)
+    print(parser.source)
+    print("-"*10)
+
+if __name__ == "__main__":
+    main()
