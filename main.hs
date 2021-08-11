09f7a4d9c09cb1abf3dee4eebcd0b1e760c6799a
dj4t9n <pandeyjatin47@gmail.com>
2021-08-11 16:16:27 +0530

    simple test circuit

--- main.hs
+++ main.hs
@@ -2,11 +2,10 @@ type Volt = Float
 type Amp = Float
 type Ohm = Float
 type Node = Int
+type Branch = ((Node, Node), Component)
 
 data Component = 
- V Float | R Float | I Float deriving(Show)
+ V Volt | R Ohm | I Amp deriving(Show)
 
-
-data Circuit = Circuit { nodes :: (Node, Node),
-                         components :: [Component]
-                       } deriving(Show)
+cir :: [Branch]
+cir = [((0, 1), V 4.4), ((1, 2), R 1), ((1, 2), R 2), ((1, 2), R 2), ((2, 3), R 3), ((3, 0), R 5)]
