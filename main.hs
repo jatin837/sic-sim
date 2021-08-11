73ff24fa05d53d37f083566add4601c85fe39b2f
dj4t9n <pandeyjatin47@gmail.com>
2021-08-11 15:43:08 +0530

    add type alias for node and datatype for circuit

--- main.hs
+++ main.hs
@@ -1,8 +1,12 @@
 type Volt = Float
 type Amp = Float
 type Ohm = Float
+type Node = Int
 
 data Component = 
- V Float | R Float | I Float
+ V Float | R Float | I Float deriving(Show)
 
 
+data Circuit = Circuit { nodes :: (Node, Node),
+                         components :: [Component]
+                       } deriving(Show)
