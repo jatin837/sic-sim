e87c7dbe9b975d853295f02bb6172343ba28bdb8
dj4t9n <pandeyjatin47@gmail.com>
2021-08-19 11:51:54 +0530

    update test case

--- main.hs
+++ main.hs
@@ -3,9 +3,16 @@ type Amp = Float
 type Ohm = Float
 type Node = Int
 type Branch = ((Node, Node), Component)
+type Circuit = [Branch]
 
 data Component = 
  V Volt | R Ohm | I Amp deriving(Show)
 
-cir :: [Branch]
-cir = [((0, 1), V 4.4), ((1, 2), R 1), ((1, 2), R 2), ((1, 2), R 2), ((2, 3), R 3), ((3, 0), R 5)]
+cir :: Circuit
+cir = [ 
+ ((0, 1), R 2), 
+ ((1, 2), V 32), 
+ ((0, 2), R 8), 
+ ((2, 3), R 4),
+ ((0, 3), V 20)
+ ]
